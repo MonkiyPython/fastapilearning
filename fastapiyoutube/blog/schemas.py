6627088be6cfa8a6ff1b943dd7386ded244decf8
MonkiyPython <rajath@monkypython.com>
2021-06-01 03:09:10 +0530

    Authentication Done JWT Generated

--- fastapiyoutube/blog/schemas.py
+++ fastapiyoutube/blog/schemas.py
@@ -1,4 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional
+
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str
+
+
+class TokenData(BaseModel):
+    username: Optional[str] = None
 
 
 class blog(BaseModel):
@@ -26,3 +36,8 @@ class ShowUser(BaseModel):
 
     class Config:
         orm_mode = True
+
+
+class Login(BaseModel):
+    username: str
+    password: str
